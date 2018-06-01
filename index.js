ad2b72114e42e02b45bcb5d44d6bbed9fcdfc690
Danilo Setúbal <danilosetubal@gmail.com>
2018-06-01 00:04:24 -0300

    Index and tokens generator included

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send('Olho na Câmara!')
+})
+
+/**
+ * Endpoints - /florianopolis
+ * Vereadores: /vereadores - Mostra a lista de vereadores
+ * Vereador ID: /vereador/:id - Busca o vereador por ID
+ * Vereador Gasto Total Período: /vereador/:id/gastos/total/:periodo
+ * Vereador Gastos <Categoria> Período: /vereador/:id/gastos/:categoria/:periodo
+ * Ranking Geral: /ranking/geral
+ * Ranking <Categoria>: /ranking/:categoria
+ *
+ */
+
+app.listen(3000, () => console.log('Falcon running on port 3000'))
---
+++ src/utils/token.js
@@ -0,0 +1,9 @@
+const rand = function () {
+  return Math.random().toString(36).substr(2)
+}
+
+module.exports = {
+  generate: function () {
+    return rand() + rand()
+  }
+}
